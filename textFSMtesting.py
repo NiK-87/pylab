c4f056a641454f6af7d0bcbeb5f21d17ea864d4c
NK <nick.kosichek@gmail.com>
2020-10-22 06:09:07 -0700

    add textfsm exercise

---
+++ textFSMtesting.py
@@ -0,0 +1,36 @@
+import os
+from netmiko import ConnectHandler
+from getpass import getpass
+from pprint import pprint
+
+
+password = os.getenv("PYNET_PASSWORD") if os.getenv("PYNET_PASSWORD") else getpass()
+
+device1 = {
+        'host': 'cisco4.lasthop.io',
+        'username': 'pyclass',
+        'password': password,
+        'device_type': 'cisco_ios'
+}
+
+net_connect = ConnectHandler(**device)
+
+print()
+
+cmds = ['show version', 'show lldp neighbors']
+
+for cmd in cmds:
+	output = net_connect.send_command(cmd, use_textfsm=True)
+	print('#'*20)
+	print(cmd)
+	print('#'*20)
+	pprint(outut)
+	print('#'*20)
+	print()
+
+	if cmd == 'show lldp neighbors':
+		print('LLDP Data Structure Type: {}'.format(type(output)))
+		print('HPE Switch Connection Port: {}'.format(output[0]['neighbor_interface']))
+
+print()
+net_connect.disconnect()
